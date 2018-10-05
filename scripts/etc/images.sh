f2fb96d624467af163d034f6ca558c5b654ea730
Nic Doye <nic.doye@alfresco.com>
2018-10-05 09:34:00 +0100

    DEPLOY-575 Base Java updates now released

--- scripts/etc/images.sh
+++ scripts/etc/images.sh
@@ -17,18 +17,18 @@ export short_name=8.5
 # The image built will be ${docker_image_repository}:${tomcat_version}-java-${java_major_version}-${java_vendor}-${docker_image_tag_suffix}
 
 # For general builds:
-# export base_image='alfresco/alfresco-base-java'
+export base_image='alfresco/alfresco-base-java'
 # If basing off unreleased quay builds:
-export base_image='quay.io/alfresco/alfresco-base-java'
+# export base_image='quay.io/alfresco/alfresco-base-java'
 
 # short_tag is 'true' if you get to be alfresco-base-tomcat:${DOCKER_IMAGE_TAG_SHORT_NAME}
 
 export -A java_8=(
-  [base_tag]=8u181-oracle-centos-7-DEPLOY-574
+  [base_tag]=8u181-oracle-centos-7-33e0cd92ffd0
   [short_tag]='true'
 )
 
 export -A java_11=(
-  [base_tag]=11-openjdk-centos-7-DEPLOY-574
+  [base_tag]=11-openjdk-centos-7-95536c8b599f
 )
 
