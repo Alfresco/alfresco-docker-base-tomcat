9eb57bd1094b94a5288e51b112f0a5723ea5adb4
svidrascu <sergiu.vidrascu@ness.com>
2019-05-09 14:18:59 +0300

    DEPLOY-685: Temporary Change image base

--- scripts/etc/images.sh
+++ scripts/etc/images.sh
@@ -24,11 +24,11 @@ export base_image='alfresco/alfresco-base-java'
 # short_tag is 'true' if you get to be alfresco-base-tomcat:${DOCKER_IMAGE_TAG_SHORT_NAME}
 
 export -A java_8=(
-  [base_tag]=8u181-oracle-centos-7-33e0cd92ffd0
+  [base_tag]=8u181-oracle-centos-7-DEPLOY-685
 )
 
 export -A java_11=(
-  [base_tag]=11.0.1-openjdk-centos-7-1fd3c4475374
+  [base_tag]=11.0.1-openjdk-centos-7-DEPLOY-685
   [short_tag]='true'
 )
 
