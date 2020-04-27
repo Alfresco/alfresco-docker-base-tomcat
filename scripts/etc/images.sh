fd32b991a73b1071f1e38e98d9540a7ee9b659d7
Sergiu Vidrascu <vsergiu@hotmail.com>
2020-04-27 23:13:13 +0300

    ACS-92: Update Java base image

--- scripts/etc/images.sh
+++ scripts/etc/images.sh
@@ -24,11 +24,11 @@ export base_image='quay.io/alfresco/alfresco-base-java'
 # short_tag is 'true' if you get to be alfresco-base-tomcat:${DOCKER_IMAGE_TAG_SHORT_NAME}
 
 export -A java_8=(
-  [base_tag]=8u181-oracle-centos-7-aee98a603d1c
+  [base_tag]=8u181-oracle-centos-7-ebbcd20c6bf4
 )
 
 export -A java_11=(
-  [base_tag]=11.0.1-openjdk-centos-7-72b88c6f1f4c
+  [base_tag]=11.0.7-openjdk-centos-7-145f5d28c8ca
   [short_tag]='true'
 )
 
